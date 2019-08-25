89daa5c9e98a6f185bdf23cccfdd94ba6591755e
AntheaHuang <a45huang@gmail.com>
2019-08-25 15:18:02 -0400

    ANQI-angular-sudoku, successfully generate solution board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,19 +31,82 @@ export class AppComponent {
     return numbers;
   }
 
-  //a backtracking way to get board
-  getRandomBoard() {
-    let numbers = new Array(9); 
- 
-    for(let i = 0; i < 9; i++) {
-      numbers[i] = [0,0,0,0,0,0,0,0,0]; 
-      numbers[i] = this.randomNineNumbers();
+  checkRow (val, x, numbers) {
+    for (let j = 0 ; j < 9 ; j++) {
+      if (numbers[x][j] == val) {
+        return false;
+      }
+    }
+    return true;
+  }
+  checkCol (val, y, numbers) {
+    for (let i = 0 ; i < 9 ; i++) {
+      if (numbers[i][y] == val) {
+        return false;
+      }
+    }
+    return true;
+  }
+  checkPalace (val, x, y, numbers) {
+    let middleCoords = [
+      [1,1], [1,4], [1,7],
+      [4,1], [4,4], [4,7],
+      [7,1], [7,4], [7,7]
+    ];
+    let position = [1,1];
+    for (let i = 1 ; i < 9 ; i++) {
+      if (Math.abs(x-middleCoords[i][0]) + Math.abs(y-middleCoords[i][1]) < 
+        Math.abs(x-position[0]) + Math.abs(y-position[1])) {
+          position = middleCoords[i];
+      }
     }
 
-    //backtracking
-
-    return numbers;
+    for (let i = -1 ; i < 2 ; i++) {
+      for (let j = -1 ; j < 2; j++) {
+        if (numbers[position[0]+i][position[1]+j] == val) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  isValid (val, i, numbers) {
+    return this.checkRow(val, Math.floor(i/9), numbers) &&
+      this.checkCol(val, i%9, numbers) &&
+      this.checkPalace(val, Math.floor(i/9), i%9, numbers);
+  }
+  //i from 0 - 80
+  backTrack(i, numbers) {
+    let x = Math.floor(i/9), y = i%9;
+    if (i == 81) {
+      return true;
+    } else if(numbers[x][y] != 0) {
+      return this.backTrack(i+1, numbers);
+    } else {
+      let candidates = this.randomNineNumbers();
+      for (let j = 0 ; j < 9 ; j ++) {
+        if (this.isValid(candidates[j], i, numbers)) {
+          numbers[x][y] = candidates[j];
+          if (this.backTrack(i+1, numbers)) {
+            return true;
+          }
+          numbers[x][y] = 0;
+        }
+      }
+    }
   }
 
+    //a backtracking way to get board
+  getRandomBoard() {
+    let numbers = new Array(9); 
+   
+    for(let i = 0; i < 9; i++) {
+      numbers[i] = [0,0,0,0,0,0,0,0,0]; 
+    }
+  
+    this.backTrack(0, numbers);
+  
+    return numbers;
+  }
   //maybe I can create a tricky way, which is faster but less randomly
 }
